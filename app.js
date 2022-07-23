e7babbec9a5a0f2a47be1c03ab7296bd47e53277
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2022-07-23 17:29:56 +0700

    call API get weather

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
-async function callAPI() {
-  const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,windspeed_10m");
+const $search = document.querySelector('.search');
+const $city = document.querySelector('.city');
+const $country = document.querySelector('.country');
+const $temp = document.querySelector('.temp');
+const $status = document.querySelector('.status');
+const $vision = document.querySelector('.vision span');
+const $wind = document.querySelector('.wind span');
+const $humidity = document.querySelector('.humidity span');
+const $time = document.querySelector('.date-time');
+const $content = document.querySelector('.content');
 
-  const jsonResponse = await response.json();
-  console.log('vao 1', jsonResponse);
+let apiKey = 'd2b5366adef8fe3416fa9347842d3831';
+let units = 'metric';// don vi do nhiet do
+let lang = 'vi';
+
+async function changeWeather() {
+  let city = $search.value.trim();
+  $search.value = '';
+
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}&lang=vi`;
+  const response = await fetch(apiURL);
+  const data = await response.json();
+  console.log(data);
+
+  if(data.cod == 200) {
+    $content.classList.remove('hide');
+
+    $city.innerText = data.name;
+    $country.innerText = data.sys.country;
+    $temp.innerText = data.main.temp.toFixed(0); // lam tron chu so
+    $status.innerText = data.weather[0].description;
+    $vision.innerText = data.visibility/1000 + 'm';
+    $wind.innerText = data.wind.speed + 'm/s';
+    $humidity.innerText = data.main.humidity + '%';
+    $time.innerText = new Date().toLocaleString('vi');
+  }
+  else {
+    $content.classList.add('hide');
+  }
 }
+changeWeather();
 
-callAPI();
+$search.addEventListener('keypress', function(e) {
+  if(e.code === 'Enter') {
+    changeWeather();
+  }
+})
