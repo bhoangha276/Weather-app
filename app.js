defd1ff682099eee196cb52c32498aceca0c8892
HaHoang <82991310+bhoangha276@users.noreply.github.com>
2022-07-24 17:49:18 +0700

    change background according to temperature

--- app.js
+++ app.js
@@ -8,40 +8,55 @@ const $wind = document.querySelector('.wind span');
 const $humidity = document.querySelector('.humidity span');
 const $time = document.querySelector('.date-time');
 const $content = document.querySelector('.content');
+const $body = document.querySelector('body');
 
 let apiKey = 'd2b5366adef8fe3416fa9347842d3831';
 let units = 'metric';// don vi do nhiet do
 let lang = 'vi';
 
-async function changeWeather() {
-  let city = $search.value.trim();
+async function changeWeather(city) {
   $search.value = '';
 
   const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}&lang=vi`;
   const response = await fetch(apiURL);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
 
   if(data.cod == 200) {
     $content.classList.remove('hide');
 
-    $city.innerText = data.name;
+    let temp = Math.round(data.main.temp);
+    $city.innerText = data.name + ',';
     $country.innerText = data.sys.country;
-    $temp.innerText = data.main.temp.toFixed(0); // lam tron chu so
+    $temp.innerText = temp; // lam tron chu so
     $status.innerText = data.weather[0].description;
     $vision.innerText = data.visibility/1000 + 'm';
     $wind.innerText = data.wind.speed + 'm/s';
     $humidity.innerText = data.main.humidity + '%';
     $time.innerText = new Date().toLocaleString('vi');
+
+    if(temp >= 25) {
+      $body.setAttribute('class', 'hot');
+    }
+    if(temp <= 22) {
+      $body.setAttribute('class', 'warm');
+    }
+    if(temp <= 19) {
+      $body.setAttribute('class', 'cold');
+    }
   }
   else {
     $content.classList.add('hide');
   }
 }
-changeWeather();
 
 $search.addEventListener('keypress', function(e) {
   if(e.code === 'Enter') {
-    changeWeather();
+    let city = $search.value.trim();
+    changeWeather(city);
+    localStorage.setItem('location', city);
   }
 })
+
+var local = localStorage.getItem("location");
+changeWeather(local);
